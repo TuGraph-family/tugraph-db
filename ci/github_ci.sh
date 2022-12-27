75cc95977970fff958c56dec33d2be12778073fa
qishipengqsp <qishipeng.qsp@antgroup.com>
2022-12-27 16:15:34 +0800

    test v3

---
+++ ci/github_ci.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+cd $WORKSPACE
+pwd
+ls -al
+
+# cpplint check
+bash ./cpplint/check_all.sh
+
+# build deps
+cd deps
+SKIP_WEB=1 bash ./build_deps.sh -j6
+
+# build tugraph
+cd $WORKSPACE
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Coverage -DENABLE_INTERNAL_BUILD=1
+make -j6
+
+# unittest
+mkdir -p $WORKSPACE/testresult/gtest/
+cd $WORKSPACE/build/output
+OMP_NUM_THREADS=8 ./unit_test --gtest_output=xml:$WORKSPACE/testresult/gtest/
+rm -rf testdb* .import_tmp
+
+# integrate tests
+cd $WORKSPACE/build/output
+cp ../../src/client/python/TuGraphClient/TuGraphClient.py .
+cp -r ../../test/integration/* ./
+pytest ./
+
+# codecov
+cd $WORKSPACE
+bash ./ci/codecov.sh $WORKSPACE/build $WORKSPACE/testresult $CODECOV_TOKEN
+python3  ./ci/lcov_cobertura.py $WORKSPACE/testresult/coverage.info --output $WORKSPACE/testresult/coverage.xml --demangle
