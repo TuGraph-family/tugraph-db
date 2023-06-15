5f7e1f9292b6e741b3ea2e43c70d4b4f3246ae4a
antkiller996 <eadrenking@outlook.com>
2023-06-15 17:05:31 +0800

    always compile c bindings

--- Options.cmake
+++ Options.cmake
@@ -89,12 +89,6 @@ else (ENABLE_FULLTEXT_INDEX)
     message("Fulltext index is disabled.")
 endif (ENABLE_FULLTEXT_INDEX)
 
-option(ENABLE_C_BINDINGS "Enable build c bindings." ON)
-if (ENABLE_C_BINDINGS)
-    message("C Bindings support is enabled.")
-else (ENABLE_C_BINDINGS)
-    message("C Bindings support is disabled.")
-endif (ENABLE_C_BINDINGS)
 
 # Detect build type, fallback to release and throw a warning if use didn't specify any
 if (NOT CMAKE_BUILD_TYPE)
