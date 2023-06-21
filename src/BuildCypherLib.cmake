cbd4eb70197c03944e530d3a78ae28ad97d8970b
PB19111683 <angels@mail.ustc.edu.cn>
2023-06-21 10:04:02 +0800

    schema rewrite

--- src/BuildCypherLib.cmake
+++ src/BuildCypherLib.cmake
@@ -3,6 +3,8 @@ cmake_minimum_required(VERSION 3.1)
 find_package(PythonLibs REQUIRED)
 
 #antlr4-runtime
+find_package(antlr4-runtime REQUIRED)
+include_directories( ${ANTLR4_INCLUDE_DIR} )
 set(ANTRL4_LIBRARY antlr4-runtime.a)
 
 set(TARGET_LGRAPH_CYPHER_LIB lgraph_cypher_lib)
