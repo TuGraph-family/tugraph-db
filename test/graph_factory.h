76c603ba0b156466602b23d4d993a84d57efca46
PB19111683 <angels@mail.ustc.edu.cn>
2023-07-25 20:04:26 +0800

    yago_with_constraints

--- test/graph_factory.h
+++ test/graph_factory.h
@@ -24,125 +24,7 @@
 
 #include "lgraph/lgraph.h"
 
-class GraphFactory {
- public:
-    static void create_snapshot(const std::string& dir = "./testdb") {
-        fma_common::FileSystem::GetFileSystem(dir).RemoveDir(dir);
-        lgraph_api::Galaxy galaxy(dir, lgraph::_detail::DEFAULT_ADMIN_NAME,
-                                  lgraph::_detail::DEFAULT_ADMIN_PASS, false, true);
-        lgraph_api::GraphDB db = galaxy.OpenGraph("default");
-        // add schemas
-        std::string v_label_1("person");
-        std::vector<lgraph::FieldSpec> v_fds_1 = {
-            {"uid", lgraph::FieldType::INT64, false},  {"name", lgraph::FieldType::STRING, false},
-            {"age", lgraph::FieldType::INT8, false},   {"age16", lgraph::FieldType::INT16, true},
-            {"age32", lgraph::FieldType::INT32, true}, {"agef", lgraph::FieldType::FLOAT, true},
-            {"aged", lgraph::FieldType::DOUBLE, true}};
-        std::string v_label_2("software");
-        std::vector<lgraph::FieldSpec> v_fds_2 = {{"uid", lgraph::FieldType::INT64, false},
-                                                  {"name", lgraph::FieldType::STRING, false},
-                                                  {"lang", lgraph::FieldType::STRING, false}};
-        std::string e_label_1("knows");
-        std::vector<lgraph::FieldSpec> e_fds_1 = {{"weight", lgraph::FieldType::FLOAT, false},
-                                                  {"since", lgraph::FieldType::INT32, true}};
-        std::string e_label_2("created");
-        std::vector<lgraph::FieldSpec> e_fds_2 = {{"weight", lgraph::FieldType::FLOAT, false}};
-        UT_ASSERT(db.AddVertexLabel(v_label_1, v_fds_1, lgraph::VertexOptions("uid")));
-        UT_ASSERT(db.AddVertexLabel(v_label_2, v_fds_2, lgraph::VertexOptions("uid")));
-        UT_ASSERT(db.AddEdgeLabel(e_label_1, e_fds_1, {}));
-        UT_ASSERT(db.AddEdgeLabel(e_label_2, e_fds_2, {}));
-
-        auto txn = db.CreateWriteTxn();
-        // add vertex
-        txn.AddVertex(
-            v_label_1,
-            std::vector<std::string>{"uid", "name", "age", "age16", "age32", "agef", "aged"},
-            std::vector<std::string>{"1", "marko", "29", "23", "22", "5.0", "6.0"});
-        txn.AddVertex(
-            v_label_1,
-            std::vector<std::string>{"uid", "name", "age", "age16", "age32", "agef", "aged"},
-            std::vector<std::string>{"2", "vadas", "27", "23", "34", "23", "232.9"});
-        txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
-                               std::vector<std::string>{"3", "lop", "java"});
-        txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
-                               std::vector<std::string>{"4", "josh", "32"});
-        txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
-                               std::vector<std::string>{"5", "ripple", "java"});
-        txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
-                               std::vector<std::string>{"6", "peter", "35"});
-        // add edge
-        txn.Commit();
-    }
-
-    static void create_modern(const std::string& dir = "./testdb") {
-        fma_common::FileSystem::GetFileSystem(dir).RemoveDir(dir);
-        lgraph_api::Galaxy galaxy(dir, lgraph::_detail::DEFAULT_ADMIN_NAME,
-                                  lgraph::_detail::DEFAULT_ADMIN_PASS, false, true);
-        lgraph_api::GraphDB db = galaxy.OpenGraph("default");
-        // add schemas
-        std::string v_label_1("person");
-        std::vector<lgraph::FieldSpec> v_fds_1 = {{"uid", lgraph::FieldType::INT64, false},
-                                                  {"name", lgraph::FieldType::STRING, false},
-                                                  {"age", lgraph::FieldType::INT8, false}};
-        std::string v_label_2("software");
-        std::vector<lgraph::FieldSpec> v_fds_2 = {{"uid", lgraph::FieldType::INT64, false},
-                                                  {"name", lgraph::FieldType::STRING, false},
-                                                  {"lang", lgraph::FieldType::STRING, false}};
-        std::string e_label_1("knows");
-        std::vector<lgraph::FieldSpec> e_fds_1 = {{"weight", lgraph::FieldType::FLOAT, false},
-                                                  {"since", lgraph::FieldType::INT32, true}};
-        std::string e_label_2("created");
-        std::vector<lgraph::FieldSpec> e_fds_2 = {{"weight", lgraph::FieldType::FLOAT, false}};
-        UT_ASSERT(db.AddVertexLabel(v_label_1, v_fds_1, lgraph::VertexOptions("uid")));
-        UT_ASSERT(db.AddVertexLabel(v_label_2, v_fds_2, lgraph::VertexOptions("uid")));
-        UT_ASSERT(db.AddEdgeLabel(e_label_1, e_fds_1, {}));
-        UT_ASSERT(db.AddEdgeLabel(e_label_2, e_fds_2, {}));
-
-        auto txn = db.CreateWriteTxn();
-        // add vertex
-        lgraph::VertexId vid[6];
-        vid[0] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
-                               std::vector<std::string>{"1", "marko", "29"});
-        vid[1] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
-                               std::vector<std::string>{"2", "vadas", "27"});
-        vid[2] = txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
-                               std::vector<std::string>{"3", "lop", "java"});
-        vid[3] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
-                               std::vector<std::string>{"4", "josh", "32"});
-        vid[4] = txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
-                               std::vector<std::string>{"5", "ripple", "java"});
-        vid[5] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
-                               std::vector<std::string>{"6", "peter", "35"});
-        // add edge
-        txn.AddEdge(vid[0], vid[1], e_label_1, std::vector<std::string>{"weight", "since"},
-                    std::vector<std::string>{"0.5", "2018"});
-        txn.AddEdge(vid[0], vid[3], e_label_1, std::vector<std::string>{"weight", "since"},
-                    std::vector<std::string>{"1.0", "1998"});
-        txn.AddEdge(vid[0], vid[2], e_label_2, std::vector<std::string>{"weight"},
-                    std::vector<std::string>{"0.4"});
-        txn.AddEdge(vid[3], vid[4], e_label_2, std::vector<std::string>{"weight"},
-                    std::vector<std::string>{"1.0"});
-        txn.AddEdge(vid[3], vid[2], e_label_2, std::vector<std::string>{"weight"},
-                    std::vector<std::string>{"0.4"});
-        txn.AddEdge(vid[5], vid[2], e_label_2, std::vector<std::string>{"weight"},
-                    std::vector<std::string>{"0.2"});
-        txn.Commit();
-    }
-
-    static void CreateCsvFiles(const std::map<std::string, std::string>& data) {
-        fma_common::OutputFmaStream stream;
-        for (auto& kv : data) {
-            const std::string& file_name = kv.first;
-            const std::string& data = kv.second;
-            stream.Open(file_name);
-            stream.Write(data.data(), data.size());
-            stream.Close();
-            UT_LOG() << file_name << " created";
-        }
-    }
-
-    static void WriteYagoFiles() {
-        static const std::map<std::string, std::string> data = {
+static const std::map<std::string, std::string> yago_data = {
             {"yago.conf", R"(
 {
     "schema": [
@@ -350,12 +232,130 @@ Lindsay Lohan,The Parent Trap,Halle/Annie
 Liam Neeson,Batman Begins,Henri Ducard
 )"}};
 
-        CreateCsvFiles(data);
+class GraphFactory {
+ public:
+    static void create_snapshot(const std::string& dir = "./testdb") {
+        fma_common::FileSystem::GetFileSystem(dir).RemoveDir(dir);
+        lgraph_api::Galaxy galaxy(dir, lgraph::_detail::DEFAULT_ADMIN_NAME,
+                                  lgraph::_detail::DEFAULT_ADMIN_PASS, false, true);
+        lgraph_api::GraphDB db = galaxy.OpenGraph("default");
+        // add schemas
+        std::string v_label_1("person");
+        std::vector<lgraph::FieldSpec> v_fds_1 = {
+            {"uid", lgraph::FieldType::INT64, false},  {"name", lgraph::FieldType::STRING, false},
+            {"age", lgraph::FieldType::INT8, false},   {"age16", lgraph::FieldType::INT16, true},
+            {"age32", lgraph::FieldType::INT32, true}, {"agef", lgraph::FieldType::FLOAT, true},
+            {"aged", lgraph::FieldType::DOUBLE, true}};
+        std::string v_label_2("software");
+        std::vector<lgraph::FieldSpec> v_fds_2 = {{"uid", lgraph::FieldType::INT64, false},
+                                                  {"name", lgraph::FieldType::STRING, false},
+                                                  {"lang", lgraph::FieldType::STRING, false}};
+        std::string e_label_1("knows");
+        std::vector<lgraph::FieldSpec> e_fds_1 = {{"weight", lgraph::FieldType::FLOAT, false},
+                                                  {"since", lgraph::FieldType::INT32, true}};
+        std::string e_label_2("created");
+        std::vector<lgraph::FieldSpec> e_fds_2 = {{"weight", lgraph::FieldType::FLOAT, false}};
+        UT_ASSERT(db.AddVertexLabel(v_label_1, v_fds_1, lgraph::VertexOptions("uid")));
+        UT_ASSERT(db.AddVertexLabel(v_label_2, v_fds_2, lgraph::VertexOptions("uid")));
+        UT_ASSERT(db.AddEdgeLabel(e_label_1, e_fds_1, {}));
+        UT_ASSERT(db.AddEdgeLabel(e_label_2, e_fds_2, {}));
+
+        auto txn = db.CreateWriteTxn();
+        // add vertex
+        txn.AddVertex(
+            v_label_1,
+            std::vector<std::string>{"uid", "name", "age", "age16", "age32", "agef", "aged"},
+            std::vector<std::string>{"1", "marko", "29", "23", "22", "5.0", "6.0"});
+        txn.AddVertex(
+            v_label_1,
+            std::vector<std::string>{"uid", "name", "age", "age16", "age32", "agef", "aged"},
+            std::vector<std::string>{"2", "vadas", "27", "23", "34", "23", "232.9"});
+        txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
+                               std::vector<std::string>{"3", "lop", "java"});
+        txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
+                               std::vector<std::string>{"4", "josh", "32"});
+        txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
+                               std::vector<std::string>{"5", "ripple", "java"});
+        txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
+                               std::vector<std::string>{"6", "peter", "35"});
+        // add edge
+        txn.Commit();
     }
 
-static void WriteYagoFilesWithConstraints() {
-        static const std::map<std::string, std::string> data = {
-            {"yago.conf", R"(
+    static void create_modern(const std::string& dir = "./testdb") {
+        fma_common::FileSystem::GetFileSystem(dir).RemoveDir(dir);
+        lgraph_api::Galaxy galaxy(dir, lgraph::_detail::DEFAULT_ADMIN_NAME,
+                                  lgraph::_detail::DEFAULT_ADMIN_PASS, false, true);
+        lgraph_api::GraphDB db = galaxy.OpenGraph("default");
+        // add schemas
+        std::string v_label_1("person");
+        std::vector<lgraph::FieldSpec> v_fds_1 = {{"uid", lgraph::FieldType::INT64, false},
+                                                  {"name", lgraph::FieldType::STRING, false},
+                                                  {"age", lgraph::FieldType::INT8, false}};
+        std::string v_label_2("software");
+        std::vector<lgraph::FieldSpec> v_fds_2 = {{"uid", lgraph::FieldType::INT64, false},
+                                                  {"name", lgraph::FieldType::STRING, false},
+                                                  {"lang", lgraph::FieldType::STRING, false}};
+        std::string e_label_1("knows");
+        std::vector<lgraph::FieldSpec> e_fds_1 = {{"weight", lgraph::FieldType::FLOAT, false},
+                                                  {"since", lgraph::FieldType::INT32, true}};
+        std::string e_label_2("created");
+        std::vector<lgraph::FieldSpec> e_fds_2 = {{"weight", lgraph::FieldType::FLOAT, false}};
+        UT_ASSERT(db.AddVertexLabel(v_label_1, v_fds_1, lgraph::VertexOptions("uid")));
+        UT_ASSERT(db.AddVertexLabel(v_label_2, v_fds_2, lgraph::VertexOptions("uid")));
+        UT_ASSERT(db.AddEdgeLabel(e_label_1, e_fds_1, {}));
+        UT_ASSERT(db.AddEdgeLabel(e_label_2, e_fds_2, {}));
+
+        auto txn = db.CreateWriteTxn();
+        // add vertex
+        lgraph::VertexId vid[6];
+        vid[0] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
+                               std::vector<std::string>{"1", "marko", "29"});
+        vid[1] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
+                               std::vector<std::string>{"2", "vadas", "27"});
+        vid[2] = txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
+                               std::vector<std::string>{"3", "lop", "java"});
+        vid[3] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
+                               std::vector<std::string>{"4", "josh", "32"});
+        vid[4] = txn.AddVertex(v_label_2, std::vector<std::string>{"uid", "name", "lang"},
+                               std::vector<std::string>{"5", "ripple", "java"});
+        vid[5] = txn.AddVertex(v_label_1, std::vector<std::string>{"uid", "name", "age"},
+                               std::vector<std::string>{"6", "peter", "35"});
+        // add edge
+        txn.AddEdge(vid[0], vid[1], e_label_1, std::vector<std::string>{"weight", "since"},
+                    std::vector<std::string>{"0.5", "2018"});
+        txn.AddEdge(vid[0], vid[3], e_label_1, std::vector<std::string>{"weight", "since"},
+                    std::vector<std::string>{"1.0", "1998"});
+        txn.AddEdge(vid[0], vid[2], e_label_2, std::vector<std::string>{"weight"},
+                    std::vector<std::string>{"0.4"});
+        txn.AddEdge(vid[3], vid[4], e_label_2, std::vector<std::string>{"weight"},
+                    std::vector<std::string>{"1.0"});
+        txn.AddEdge(vid[3], vid[2], e_label_2, std::vector<std::string>{"weight"},
+                    std::vector<std::string>{"0.4"});
+        txn.AddEdge(vid[5], vid[2], e_label_2, std::vector<std::string>{"weight"},
+                    std::vector<std::string>{"0.2"});
+        txn.Commit();
+    }
+
+    static void CreateCsvFiles(const std::map<std::string, std::string>& data) {
+        fma_common::OutputFmaStream stream;
+        for (auto& kv : data) {
+            const std::string& file_name = kv.first;
+            const std::string& data = kv.second;
+            stream.Open(file_name);
+            stream.Write(data.data(), data.size());
+            stream.Close();
+            UT_LOG() << file_name << " created";
+        }
+    }
+
+    static void WriteYagoFiles() {
+        CreateCsvFiles(yago_data);
+    }
+
+    static void WriteYagoFilesWithConstraints() {
+        auto yago_data_with_constraints = yago_data;
+        yago_data_with_constraints.at("yago.conf") = R"(
 {
     "schema": [
         {
@@ -397,6 +397,7 @@ static void WriteYagoFilesWithConstraints() {
             "label" : "BORN_IN", 
             "type" : "EDGE",
             "properties" : [
+                {"name" : "reg_time", "type":"DATETIME", "optional":true},
                 {"name" : "weight", "type":"FLOAT", "optional":true}
             ],
             "constraints": [["Person", "City"]]
@@ -461,7 +462,7 @@ static void WriteYagoFilesWithConstraints() {
             "label" : "BORN_IN",
             "SRC_ID" : "Person",
             "DST_ID" : "City",
-            "columns" : ["SRC_ID","DST_ID","weight"]
+            "columns" : ["SRC_ID","DST_ID","reg_time","weight"]
         },
         {
             "path" : "directed.csv",
@@ -489,85 +490,9 @@ static void WriteYagoFilesWithConstraints() {
         }
     ]
 }
-                    )"},
+                    )";
 
-            {"person.csv",
-             R"(Rachel Kempson,1910
-Michael Redgrave,1908
-Vanessa Redgrave,1937
-Corin Redgrave,1939
-Liam Neeson,1952
-Natasha Richardson,1963
-Richard Harris,1930
-Dennis Quaid,1954
-Lindsay Lohan,1986
-Jemma Redgrave,1965
-Roy Redgrave,1873
-John Williams,1932
-Christopher Nolan,1970
-)"},
-
-            {"city.csv",
-             R"(New York
-London
-Houston
-)"},
-
-            {"film.csv",
-             R"("Goodbye, Mr. Chips"
-Batman Begins
-Harry Potter and the Sorcerer's Stone
-The Parent Trap
-Camelot
-)"},
-
-            {"has_child.csv",
-             R"(Rachel Kempson,Vanessa Redgrave
-Rachel Kempson,Corin Redgrave
-Michael Redgrave,Vanessa Redgrave
-Michael Redgrave,Corin Redgrave
-Corin Redgrave,Jemma Redgrave
-Vanessa Redgrave,Natasha Richardson
-Roy Redgrave,Michael Redgrave
-)"},
-
-            {"married.csv",
-             R"(Rachel Kempson,Michael Redgrave
-Michael Redgrave,Rachel Kempson
-Natasha Richardson,Liam Neeson
-Liam Neeson,Natasha Richardson
-)"},
-
-            {"born_in.csv",
-             R"(Vanessa Redgrave,London,20.21
-Natasha Richardson,London,20.18
-Christopher Nolan,London,19.93
-Dennis Quaid,Houston,19.11
-Lindsay Lohan,New York,20.62
-John Williams,New York,20.55
-)"},
-
-            {"directed.csv",
-             R"(Christopher Nolan,Batman Begins
-)"},
-
-            {"wrote.csv",
-             R"(John Williams,Harry Potter and the Sorcerer's Stone
-John Williams,"Goodbye, Mr. Chips"
-)"},
-
-            {"acted_in.csv",
-             R"(Michael Redgrave,"Goodbye, Mr. Chips",The Headmaster
-Vanessa Redgrave,Camelot,Guenevere
-Richard Harris,Camelot,King Arthur
-Richard Harris,Harry Potter and the Sorcerer's Stone,Albus Dumbledore
-Natasha Richardson,The Parent Trap,Liz James
-Dennis Quaid,The Parent Trap,Nick Parker
-Lindsay Lohan,The Parent Trap,Halle/Annie
-Liam Neeson,Batman Begins,Henri Ducard
-)"}};
-
-        CreateCsvFiles(data);
+        CreateCsvFiles(yago_data_with_constraints);
     }
 
     /**
