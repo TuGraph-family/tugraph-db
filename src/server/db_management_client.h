166892efcc6ad6cadcabc488074f354b0fe9709a
SonglinLyu <lyus@usc.edu>
2023-10-18 19:02:54 +0000

    fix pr comments

--- src/server/db_management_client.h
+++ src/server/db_management_client.h
@@ -23,6 +23,9 @@
 namespace lgraph {
 namespace db_management = com::antgroup::tugraph;
 class DBManagementClient {
+  // TODO: get host and port from license
+  // TODO: change name of ReadJobResult
+  // TODO: kill all process after ut
  private:
   bool heartbeat_ = false;
   static const int detect_freq_ = 5;
@@ -33,32 +36,137 @@ class DBManagementClient {
  public:
   DBManagementClient();
 
+  /**
+    * @brief   Get an instance of DBManagementClient.
+    */
   static DBManagementClient& GetInstance();
 
+  /**
+    * @brief   Init brpc channel.
+    *
+    * @param   server   The server address of db management, like "localhost:6091".
+    */
   void InitChannel(std::string sever);
 
+  /**
+    * @brief   set heartbeat of db management.
+    *
+    * @param   heartbeat   true if connected, false if not connected.
+    */
   void SetHeartbeat(bool heartbeat);
 
+  /**
+    * @brief   Get heartbeat of db management.
+    *
+    * @returns   true if connected, false if not.
+    */
   bool GetHeartbeat();
-
+  
+  /**
+    * @brief   Get brpc stub for heart detection.
+    c
+    */
   db_management::HeartbeatService_Stub& GetHeartbeatStub();
 
+  /**
+    * @brief   Get brpc stub for job management.
+    *
+    * @returns   job management stub.
+    */
   db_management::JobManagementService_Stub& GetJobStub();
 
+  /**
+    * @brief  Heartbeat detection function. Runs in a detached thread.
+    *
+    */
   static void DetectHeartbeat();
 
+  /**
+    * @brief   create a job record in db management.
+    *
+    * @param   host   host address of current db.
+    *
+    * @param   port   port of current db.
+    *
+    * @param   start_time   start time of a job, echo time in ms, int64.
+    *
+    * @param   period   job period type, PERIODIC, IMMEDIATE, DELAYED.
+    *
+    * @param   name   name of this job.
+    *
+    * @param   type   type of this job.
+    *
+    * @param   user   user of db who create this job.
+    *
+    * @param   create_time   create time of a job, echo time in ms, int64.
+    *
+    * @returns   unique job_id for created job record.
+    */
   int CreateJob(std::string host, std::string port, std::int64_t start_time, std::string period,
                 std::string name, std::string type, std::string user, std::int64_t create_time);
 
+  /**
+    * @brief   update a job record in db management with job_id.
+    *
+    * @param   host   host address of current db.
+    *
+    * @param   port   port of current db.
+    *
+    * @param   status   job status, pending, success, failed.
+    *
+    * @param   runtime   total runtime of a job, echo time in ms, int64.
+    *
+    * @param   result   result of job in string.
+    */
   void UpdateJob(std::string host, std::string port, int job_id, std::string status,
                  std::int64_t runtime, std::string result);
 
+  /**
+    * @brief   read all job status in db management.
+    *
+    * @param   host   host address of current db.
+    *
+    * @param   port   port of current db.
+    *
+    * @returns   a list for all job status in db management.
+    */
   std::vector<db_management::Job> ReadJob(std::string host, std::string port);
 
+  /**
+    * @brief   read job status with given job_id in db management.
+    *
+    * @param   host   host address of current db.
+    *
+    * @param   port   port of current db.
+    *
+    * @param   job_id   job_id of the job you want to read.
+    *
+    * @returns   job.
+    */
   db_management::Job ReadJob(std::string host, std::string port, int job_id);
 
+  /**
+    * @brief   read job result with given job_id in db management.
+    *
+    * @param   host   host address of current db.
+    *
+    * @param   port   port of current db.
+    *
+    * @param   job_id   job_id of the job you want to read.
+    *
+    * @returns   job result.
+    */
   db_management::JobResult ReadJobResult(std::string host, std::string port, int job_id);
 
+  /**
+    * @brief   delete a job record with given job_id in db management.
+    *
+    * @param   host   host address of current db.
+    *
+    * @param   port   port of current db.
+    *
+    * @param   job_id   job_id of the job you want to read.
+    */
   void DeleteJob(std::string host, std::string port, int job_id);
 };
 }  // namespace lgraph
