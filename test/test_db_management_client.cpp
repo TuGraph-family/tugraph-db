3b8f657f68d2ac18c242a9d140d64cdabe1d8942
SonglinLyu <lyus@usc.edu>
2023-10-22 03:09:59 +0000

    fix ut

--- test/test_db_management_client.cpp
+++ test/test_db_management_client.cpp
@@ -95,7 +95,7 @@ TEST_F(TestDBManagementClient, DBManagementClient) {
     cmd = "cd " + db_management_folder + " && " + "bash ut_start.sh";
     rt = system(cmd.c_str());
     UT_EXPECT_EQ(rt, 0);
-    // sleep to wait db management start
+    // sleep and wait db management start
     fma_common::SleepS(300);
 
     // test crud
