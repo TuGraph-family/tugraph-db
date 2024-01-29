aef054bf5d60991b12eef1dece059062243c24f7
qishipengqsp <shepherdkingqsp@gmail.com>
2024-01-29 10:41:46 +0800

    update copyright to 2024

--- test/test_http_tasks.cpp
+++ test/test_http_tasks.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 AntGroup CO., Ltd.
+ * Copyright 2024 AntGroup CO., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
