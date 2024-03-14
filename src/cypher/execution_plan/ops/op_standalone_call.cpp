175371da841618594e118aed5a62beb82058f290
Wang Zhiyong <zhiyong4wang@foxmail.com>
2024-03-14 09:08:34 +0000

    fix parameter error in cypher plugin call

--- src/cypher/execution_plan/ops/op_standalone_call.cpp
+++ src/cypher/execution_plan/ops/op_standalone_call.cpp
@@ -93,6 +93,34 @@ cypher::OpBase::OpResult cypher::StandaloneCall::RealConsume(RTContext *ctx) {
         std::vector<std::string> _yield_items;
         std::transform(yield_items.cbegin(), yield_items.cend(), std::back_inserter(_yield_items),
                        [](const auto &item) { return item.first; });
+        for (auto& p : parameters) {
+            if (p.type == parser::Expression::DataType::PARAMETER) {
+                auto it = ctx->param_tab_.find(p.String());
+                if (it == ctx->param_tab_.end())
+                    throw lgraph::CypherException("invalid parameter: " + p.String());
+                if (it->second.type != cypher::FieldData::SCALAR) {
+                    throw lgraph::CypherException("parameter with wrong type: " + p.String());
+                }
+                if (it->second.scalar.IsInt64()) {
+                    p.type = parser::Expression::DataType::INT;
+                    p.data = it->second.scalar.AsInt64();
+                } else if (it->second.scalar.IsString()) {
+                    p.type = parser::Expression::DataType::STRING;
+                    p.data = std::make_shared<std::string>(it->second.scalar.AsString());
+                } else if (it->second.scalar.IsBool()) {
+                    p.type = parser::Expression::DataType::BOOL;
+                    p.data = it->second.scalar.AsBool();
+                } else if (it->second.scalar.IsDouble()) {
+                    p.type = parser::Expression::DataType::DOUBLE;
+                    p.data = it->second.scalar.AsDouble();
+                } else {
+                    THROW_CODE(
+                        CypherParameterTypeError,
+                        "Call procedure, unexpected parameter type, parameter: {}, type: {}",
+                        it->first, it->second.scalar.GetType());
+                }
+            }
+        }
         procedure_->function(ctx, nullptr, parameters, _yield_items, &records);
         auto &header = ctx->result_->Header();
         for (auto &r : records) {
