4f4acd3f7e52f1683d5343dd3e247d1c6ef8ff6d
Wang Zhiyong <zhiyong4wang@foxmail.com>
2024-05-07 08:23:39 +0000

    update

--- src/core/lightning_graph.cpp
+++ src/core/lightning_graph.cpp
@@ -463,40 +463,55 @@ bool LightningGraph::_AlterLabel(
     size_t n_committed = 0;
     LabelId curr_lid = curr_schema->GetLabelId();
     if (is_vertex) {
-        // scan and modify the vertexes
-        std::unique_ptr<lgraph::graph::VertexIterator> vit(
-            new lgraph::graph::VertexIterator(graph_->GetUnmanagedVertexIterator(&txn.GetTxn())));
-        while (vit->IsValid()) {
-            Value prop = vit->GetProperty();
-            if (curr_sm->GetRecordLabelId(prop) == curr_lid) {
-                modified++;
-                if (curr_schema->DetachProperty()) {
-                    prop = curr_schema->GetDetachedVertexProperty(txn.GetTxn(), vit->GetId());
-                }
+        if (curr_schema->DetachProperty()) {
+            auto table_name = curr_schema->GetPropertyTable().Name();
+            LOG_INFO() << FMA_FMT("begin to scan detached table: {}", table_name);
+            auto kv_iter = curr_schema->GetPropertyTable().GetIterator(txn.GetTxn());
+            for (kv_iter->GotoFirstKey(); kv_iter->IsValid(); kv_iter->Next()) {
+                auto prop = kv_iter->GetValue();
                 Value new_prop = make_new_prop_and_destroy_old(prop, curr_schema, new_schema, txn);
-                vit->RefreshContentIfKvIteratorModified();
-                if (curr_schema->DetachProperty()) {
-                    curr_schema->SetDetachedVertexProperty(txn.GetTxn(), vit->GetId(), new_prop);
-                } else {
-                    vit->SetProperty(new_prop);
-                }
-                if (modified - n_committed >= commit_size) {
-#if PERIODIC_COMMIT
-                    VertexId vid = vit->GetId();
-                    vit.reset();
-                    txn.Commit();
-                    n_committed = modified;
-                    FMA_LOG() << "Committed " << n_committed << " changes.";
-                    txn = CreateWriteTxn(false, false, false);
-                    vit.reset(new lgraph::graph::VertexIterator(
-                        graph_->GetUnmanagedVertexIterator(&txn.GetTxn(), vid, true)));
-#else
-                    n_committed = modified;
-                    LOG_INFO() << "Made " << n_committed << " changes.";
-#endif
+                kv_iter->SetValue(new_prop);
+                modified++;
+                if (modified % 1000000 == 0) {
+                    LOG_INFO() << "modified: " << modified;
                 }
             }
-            vit->Next();
+            LOG_INFO() << "modified: " << modified;
+            kv_iter.reset();
+            LOG_INFO() << FMA_FMT("end to scan detached table: {}", table_name);
+        } else {
+            // scan and modify the vertexes
+            std::unique_ptr<lgraph::graph::VertexIterator> vit(
+                new lgraph::graph::VertexIterator(graph_->GetUnmanagedVertexIterator(&txn.GetTxn())));
+            while (vit->IsValid()) {
+                Value prop = vit->GetProperty();
+                if (curr_sm->GetRecordLabelId(prop) == curr_lid) {
+                    modified++;
+                    Value new_prop = make_new_prop_and_destroy_old(prop, curr_schema, new_schema, txn);
+                    vit->RefreshContentIfKvIteratorModified();
+                    if (curr_schema->DetachProperty()) {
+                        curr_schema->SetDetachedVertexProperty(txn.GetTxn(), vit->GetId(), new_prop);
+                    } else {
+                        vit->SetProperty(new_prop);
+                    }
+                    if (modified - n_committed >= commit_size) {
+    #if PERIODIC_COMMIT
+                        VertexId vid = vit->GetId();
+                        vit.reset();
+                        txn.Commit();
+                        n_committed = modified;
+                        FMA_LOG() << "Committed " << n_committed << " changes.";
+                        txn = CreateWriteTxn(false, false, false);
+                        vit.reset(new lgraph::graph::VertexIterator(
+                            graph_->GetUnmanagedVertexIterator(&txn.GetTxn(), vid, true)));
+    #else
+                        n_committed = modified;
+                        LOG_INFO() << "Made " << n_committed << " changes.";
+    #endif
+                    }
+                }
+                vit->Next();
+            }
         }
         modify_index(curr_schema, new_schema, rollback_actions, txn);
     } else {
