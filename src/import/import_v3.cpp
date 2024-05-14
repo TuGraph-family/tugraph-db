a2b8b72e9d3a84da90b725227e274c97e191dce1
lipanpan03 <41904587+lipanpan03@users.noreply.github.com>
2024-05-14 19:37:37 +0800

    add limit to unique index (#509)
    * add limit to vertex unique index
    
    * add limit to vertex unique index
    
    * add limit to vertex unique index
    
    * add limit to edge unique index
    
    * add limit to edge unique index
    
    * add limit to edge unique index
    
    * add limit to edge unique index
    
    * modify index doc

--- src/import/import_v3.cpp
+++ src/import/import_v3.cpp
@@ -161,15 +161,15 @@ void Importer::DoImportOffline() {
                                     v.name, spec.name);
                     }
                 } else if (v.is_vertex && spec.index && !spec.primary &&
-                           (spec.idxType == lgraph::IndexType::GlobalUniqueIndex ||
-                            spec.idxType == lgraph::IndexType::PairUniqueIndex)) {
+                           spec.idxType != lgraph::IndexType::NonuniqueIndex) {
                     THROW_CODE(InputError,
                         "offline import does not support to create a unique "
                                 "index [label:{}, field:{}]. You should create an index for "
                                 "an attribute column after the import is complete",
                                 v.name, spec.name);
                 } else if (!v.is_vertex && spec.index &&
-                           spec.idxType != lgraph::IndexType::GlobalUniqueIndex) {
+                           (spec.idxType == lgraph::IndexType::NonuniqueIndex ||
+                           spec.idxType == lgraph::IndexType::PairUniqueIndex)) {
                     if (db_->AddEdgeIndex(v.name, spec.name, spec.idxType)) {
                         if (!config_.import_online) {
                             LOG_INFO() << FMA_FMT("Add edge index [label:{}, field:{}, type:{}]",
@@ -186,7 +186,7 @@ void Importer::DoImportOffline() {
                 } else if (!v.is_vertex && spec.index &&
                            spec.idxType == lgraph::IndexType::GlobalUniqueIndex) {
                     THROW_CODE(InputError,
-                        "offline import does not support to create a unique "
+                        "offline import does not support to create an unique "
                                 "index [label:{}, field:{}]. You should create an index for "
                                 "an attribute column after the import is complete",
                                 v.name, spec.name);
