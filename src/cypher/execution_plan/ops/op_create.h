87d7183211537dd963adf172795dd2e8ca419b73
Jant1L <1399324235@qq.com>
2024-06-26 09:00:39 +0000

    change op file

--- src/cypher/execution_plan/ops/op_create.h
+++ src/cypher/execution_plan/ops/op_create.h
@@ -30,76 +30,7 @@ class OpCreate : public OpBase {
     bool summary_ = false;
 
     void ExtractProperties(RTContext *ctx, const parser::TUP_PROPERTIES &properties,
-                           VEC_STR &fields, std::vector<lgraph::FieldData> &values) {
-        using namespace parser;
-        Expression map_literal = std::get<0>(properties);
-        CYPHER_THROW_ASSERT(map_literal.type == Expression::NA ||
-                            map_literal.type == Expression::MAP);
-        if (map_literal.type != Expression::MAP) return;
-        for (auto &prop : map_literal.Map()) {
-            fields.emplace_back(prop.first);
-            Expression p;
-            if (prop.second.type == Expression::LIST) {
-                p = prop.second;
-            } else if (prop.second.type == Expression::MAP) {
-                CYPHER_TODO();
-            } else {
-                p = prop.second;
-            }
-            ArithExprNode ae(p, *record->symbol_table);
-            auto value = ae.Evaluate(ctx, *record);
-            CYPHER_THROW_ASSERT(!value.IsNull());
-            if (value.IsArray()) {
-                std::vector<float> vec;
-                int dim = value.constant.array->size();
-                if (dim >= 4096) {
-                    throw lgraph::ReminderException(
-                        "The dimensions of a vector cannot exceed 4096.");
-                }
-                for (int i = 0; i < dim; ++i) {
-                    lgraph::FieldData FD = value.constant.array->at(i);
-                    float num;
-                    switch (FD.type) {
-                    case lgraph::FieldType::NUL:
-                        throw std::bad_cast();
-                    case lgraph::FieldType::INT8:
-                        num = static_cast<float>(FD.AsInt8());
-                        break;
-                    case lgraph::FieldType::INT16:
-                        num = static_cast<float>(FD.AsInt16());
-                        break;
-                    case lgraph::FieldType::INT32:
-                        num = static_cast<float>(FD.AsInt32());
-                        break;
-                    case lgraph::FieldType::INT64:
-                        num = static_cast<float>(FD.AsInt64());
-                        break;
-                    case lgraph::FieldType::FLOAT:
-                        num = static_cast<float>(FD.AsFloat());
-                        break;
-                    case lgraph::FieldType::DOUBLE:
-                        num = static_cast<float>(FD.AsDouble());
-                        break;
-                    case lgraph::FieldType::BOOL:
-                    case lgraph::FieldType::DATE:
-                    case lgraph::FieldType::DATETIME:
-                    case lgraph::FieldType::STRING:
-                    case lgraph::FieldType::BLOB:
-                    case lgraph::FieldType::POINT:
-                    case lgraph::FieldType::LINESTRING:
-                    case lgraph::FieldType::POLYGON:
-                    case lgraph::FieldType::SPATIAL:
-                    case lgraph::FieldType::FLOAT_VECTOR:
-                        throw std::bad_cast();
-                    }
-                    vec.emplace_back(num);
-                }
-                values.emplace_back(lgraph::FieldData(vec));
-            } else {
-                values.emplace_back(value.constant.scalar);
-            }
-        }
-    }
+                           VEC_STR &fields, std::vector<lgraph::FieldData> &values);
 
     void CreateVertex(RTContext *ctx, const parser::TUP_NODE_PATTERN &node_pattern) {
         using namespace parser;
--- src/cypher/execution_plan/ops/op_set.h
+++ src/cypher/execution_plan/ops/op_set.h
@@ -57,60 +57,7 @@ class OpSet : public OpBase {
     }
 
     void ExtractProperties(RTContext *ctx, const parser::Expression &p,
-                           std::vector<lgraph::FieldData> &values) {
-        ArithExprNode ae(p, *record->symbol_table);
-        auto val = ae.Evaluate(ctx, *record);
-        if (val.IsScalar()) {
-            values.emplace_back(val.constant.scalar);
-        } else if (val.IsArray()) {
-            std::vector<float> vec;
-            int dim = val.constant.array->size();
-            if (dim >= 4096) {
-                throw lgraph::ReminderException("The dimensions of a vector cannot exceed 4096.");
-            }
-            for (int i = 0; i < dim; ++i) {
-                lgraph::FieldData FD = val.constant.array->at(i);
-                float num;
-                switch (FD.type) {
-                case lgraph::FieldType::NUL:
-                    throw std::bad_cast();
-                case lgraph::FieldType::INT8:
-                    num = static_cast<float>(FD.AsInt8());
-                    break;
-                case lgraph::FieldType::INT16:
-                    num = static_cast<float>(FD.AsInt16());
-                    break;
-                case lgraph::FieldType::INT32:
-                    num = static_cast<float>(FD.AsInt32());
-                    break;
-                case lgraph::FieldType::INT64:
-                    num = static_cast<float>(FD.AsInt64());
-                    break;
-                case lgraph::FieldType::FLOAT:
-                    num = static_cast<float>(FD.AsFloat());
-                    break;
-                case lgraph::FieldType::DOUBLE:
-                    num = static_cast<float>(FD.AsDouble());
-                    break;
-                case lgraph::FieldType::BOOL:
-                case lgraph::FieldType::DATE:
-                case lgraph::FieldType::DATETIME:
-                case lgraph::FieldType::STRING:
-                case lgraph::FieldType::BLOB:
-                case lgraph::FieldType::POINT:
-                case lgraph::FieldType::LINESTRING:
-                case lgraph::FieldType::POLYGON:
-                case lgraph::FieldType::SPATIAL:
-                case lgraph::FieldType::FLOAT_VECTOR:
-                    throw std::bad_cast();
-                }
-                vec.emplace_back(num);
-            }
-            values.emplace_back(lgraph::FieldData(vec));
-        } else {
-            CYPHER_TODO();
-        }
-    }
+                           std::vector<lgraph::FieldData> &values);
 
     void SetVertex(RTContext *ctx,
                    const parser::Expression::EXPR_TYPE_PROPERTY &property_expression,
