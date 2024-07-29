db6c10914f3325077b674a8045f8489711960b7e
Jant1L <1399324235@qq.com>
2024-07-29 02:28:24 +0000

    fix cpplint

--- src/core/vector_index_layer.h
+++ src/core/vector_index_layer.h
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2022 AntGroup CO., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 #pragma once
 
 #include <vector>
@@ -9,26 +23,27 @@
 namespace lgraph {
 
 class VectorIndex {
-   friend class Schema;
-   friend class LightningGraph;
-   friend class Transaction;
-   friend class IndexManager;
+  friend class Schema;
+  friend class LightningGraph;
+  friend class Transaction;
+  friend class IndexManager;
 
  protected:
-   std::string label_;
-   std::string name_;
-   std::string distance_type_;
-   std::string index_type_;
-   int vec_dimension_;
-   std::vector<int> index_spec_;
-   int query_spec_;
-   VectorIndexManager vector_index_manager_;
-   std::shared_ptr<KvTable> table_;
-   bool rebuild_;
+  std::string label_;
+  std::string name_;
+  std::string distance_type_;
+  std::string index_type_;
+  int vec_dimension_;
+  std::vector<int> index_spec_;
+  int query_spec_;
+  VectorIndexManager vector_index_manager_;
+  std::shared_ptr<KvTable> table_;
+  bool rebuild_;
 
  public:
-    VectorIndex(const std::string& label, const std::string& name, const std::string& distance_type,
-                const std::string& index_type, int vec_dimension, std::vector<int> index_spec, std::shared_ptr<KvTable> table);
+    VectorIndex(const std::string& label, const std::string& name,
+                const std::string& distance_type, const std::string& index_type,
+                int vec_dimension, std::vector<int> index_spec, std::shared_ptr<KvTable> table);
 
     VectorIndex(const VectorIndex& rhs);
 
@@ -44,16 +59,16 @@ class VectorIndex {
     // get name
     std::string GetName() { return name_; }
 
-    //get distance type
+    // get distance type
     std::string GetDistanceType() { return distance_type_; }
 
-    //get index type
+    // get index type
     std::string GetIndexType() { return index_type_; }
 
-    //get vector dimension
+    // get vector dimension
     int GetVecDimension() { return vec_dimension_; }
 
-    //get the vector table
+    // get the vector table
     std::shared_ptr<KvTable> GetTable() { return table_; }
 
     // get vector_index_manager
@@ -69,7 +84,7 @@ class VectorIndex {
     virtual bool Add(const std::vector<std::vector<float>>& vectors, size_t num_vectors) = 0;
 
     // build index
-    virtual bool Build() = 0 ;
+    virtual bool Build() = 0;
 
     // serialize index
     virtual std::vector<uint8_t> Save() = 0;
@@ -78,7 +93,8 @@ class VectorIndex {
     virtual void Load(std::vector<uint8_t>& idx_bytes) = 0;
 
     // search vector in index
-    virtual bool Search(const std::vector<float> query, size_t num_results, std::vector<float>& distances, std::vector<int64_t>& indices) = 0;
+    virtual bool Search(const std::vector<float> query, size_t num_results,
+                        std::vector<float>& distances, std::vector<int64_t>& indices) = 0;
 
     static std::unique_ptr<KvTable> OpenTable(KvTransaction& txn, KvStore& store,
                                    const std::string& name, FieldType dt, IndexType type);
@@ -87,7 +103,9 @@ class VectorIndex {
 
     void CleanVectorFromTable(KvTransaction& txn);
 
-    virtual bool GetFlatSearchResult(KvTransaction& txn, const std::vector<float> query, size_t num_results, std::vector<float>& distances, std::vector<int64_t>& indices) = 0 ;
+    virtual bool GetFlatSearchResult(KvTransaction& txn, const std::vector<float> query,
+                                     size_t num_results, std::vector<float>& distances,
+                                     std::vector<int64_t>& indices) = 0;
 
 };
-} // namespace lgraph
+}  // namespace lgraph
--- src/core/vector_index_manager.cpp
+++ src/core/vector_index_manager.cpp
@@ -1,10 +1,23 @@
-// VectorIndexManager.cpp  
-#include "vector_index_manager.h"
+/**
+ * Copyright 2022 AntGroup CO., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+#include "core/vector_index_manager.h"
 
 namespace lgraph {
 
-VectorIndexManager::VectorIndexManager(size_t count)  
-    : count_(count),  indexed_(false){}
+VectorIndexManager::VectorIndexManager(size_t count)
+    : count_(count),  indexed_(false) {}
 
 VectorIndexManager::VectorIndexManager(const VectorIndexManager& rhs)
     : count_(rhs.count_),
@@ -15,15 +28,15 @@ void VectorIndexManager::addCount() {
     count_++;
 }
 
-bool VectorIndexManager::MakeVectorIndex() { 
+bool VectorIndexManager::MakeVectorIndex() {
     indexed_ = true;
-    return true; 
+    return true;
 }
 
 bool VectorIndexManager::UpdateCount(size_t DataSize) {
     count_ = count_ + DataSize;
     return true;
-} 
+}
 
 bool VectorIndexManager::WhetherUpdate() {
     if (count_ >= 10000) {
@@ -34,4 +47,4 @@ bool VectorIndexManager::WhetherUpdate() {
     }
 }
 
-}
+}  // namespace lgraph
