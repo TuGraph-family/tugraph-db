8867ea01de48487a369ba96bcf87cc32556b00ee
Jant1L <1399324235@qq.com>
2024-07-29 03:42:16 +0000

    fix cpplint

--- src/core/vector_index_manager.h
+++ src/core/vector_index_manager.h
@@ -1,12 +1,24 @@
-// VectorIndexManager.h  
+/**
+ * Copyright 2022 AntGroup CO., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
 
 #pragma once
 
-#include <unordered_map>  
-#include <mutex>  
-#include <memory>  
-#include <string>  
-#include <vector>  
+#include <unordered_map>
+#include <mutex>
+#include <memory>
+#include <string>
+#include <vector>
 
 #include "/root/tugraph-db/include/tools/lgraph_log.h"
 
@@ -14,32 +26,30 @@
 namespace lgraph {
 
 class VectorIndexManager {
-   size_t count_;
-   bool indexed_;
+  size_t count_;
+  bool indexed_;
 
-public:
+ public:
+  explicit VectorIndexManager(size_t count);
 
-   VectorIndexManager(size_t count);
+  VectorIndexManager(const VectorIndexManager& rhs);
 
-   VectorIndexManager(const VectorIndexManager& rhs);
+  VectorIndexManager(VectorIndexManager&& rhs) = delete;
 
-   VectorIndexManager(VectorIndexManager&& rhs) = delete;
+  VectorIndexManager& operator=(const VectorIndexManager& rhs) = delete;
 
-   VectorIndexManager& operator=(const VectorIndexManager& rhs) = delete;
+  VectorIndexManager& operator=(VectorIndexManager&& rhs) = delete;
 
-   VectorIndexManager& operator=(VectorIndexManager&& rhs) = delete;
+  size_t getCount() { return count_; }
 
-   size_t getCount() { return count_; }
+  bool isIndexed() { return indexed_; }
 
-   bool isIndexed() { return indexed_; }
+  void addCount();
 
-   void addCount();
+  bool MakeVectorIndex();
 
-   bool MakeVectorIndex();
+  bool UpdateCount(size_t DataSize);
 
-   bool UpdateCount(size_t DataSize);
-
-   bool WhetherUpdate();
-
+  bool WhetherUpdate();
 };
-}
+}  // namespace lgraph
