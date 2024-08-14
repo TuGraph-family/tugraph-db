5cf5db1e9e90933ce91cb45b0ee5e26ecccd978d
crx-dev2 <1594837530@qq.com>
2024-08-14 09:54:38 +0000

    louvain_procedure output primaryKeyValue

--- procedures/algo_cpp/louvain_procedure.cpp
+++ procedures/algo_cpp/louvain_procedure.cpp
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
+#include <cstddef>
+#include <string>
+#include <tuple>
+#include <unordered_map>
 #include "lgraph/olap_on_db.h"
 #include "tools/json.hpp"
 #include "./algo.h"
@@ -48,7 +52,15 @@ extern "C" bool Process(GraphDB& db, const std::string& request, std::string& re
     auto label = olapondb.AllocVertexArray<size_t>();
     auto modularity = LouvainCore(olapondb, label, active_threshold, is_sync);
     auto core_cost = get_time() - start_time;
-
+    std::unordered_map<size_t,
+                    std::tuple<std::string, std::string, std::string>> vid_primaryKey_map;
+    for (size_t i = 0; i < olapondb.NumVertices(); i++) {
+        auto vit = txn.GetVertexIterator(i, false);
+        auto vit_label = vit.GetLabel();
+        auto primary_field = txn.GetVertexPrimaryField(vit_label);
+        auto field_data = vit.GetField(primary_field);
+        vid_primaryKey_map[i] = std::make_tuple(vit_label, primary_field, field_data.ToString());
+    }
     // output
     start_time = get_time();
     if (output_file != "") {
@@ -56,11 +68,20 @@ extern "C" bool Process(GraphDB& db, const std::string& request, std::string& re
 #pragma omp parallel for
         for (size_t i = 0; i < olapondb.NumVertices(); i++) {
             if (label[i]) {
-                fprintf(fout, "%ld, %ld\n", i, label[i]);
+                fprintf(fout, "%ld, %s.%s, %s, %ld, %s.%s, %s\n",
+                i,
+                std::get<0>(vid_primaryKey_map[i]).c_str(),
+                std::get<1>(vid_primaryKey_map[i]).c_str(),
+                std::get<2>(vid_primaryKey_map[i]).c_str(),
+                label[i],
+                std::get<0>(vid_primaryKey_map[label[i]]).c_str(),
+                std::get<1>(vid_primaryKey_map[label[i]]).c_str(),
+                std::get<2>(vid_primaryKey_map[label[i]]).c_str());
             }
         }
         fclose(fout);
     }
+
     double output_cost = get_time() - start_time;
 
     json output;
