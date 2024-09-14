1d79c22da60cd0a651bff5d101189daf7150c4ed
lipanpan03 <656461146@qq.com>
2024-09-14 10:57:19 +0800

    fix vsag ut

--- test/test_vsag_index.cpp
+++ test/test_vsag_index.cpp
@@ -104,94 +104,6 @@ TEST_F(TestVsag, DeleteVectors) {
     }
 }
 
-std::mutex lock_rpc_vector;
-std::condition_variable cond_vector;
-int stage_3_vector = 0;
-lgraph::StateMachine::Config sm_config_vector;
-std::shared_ptr<lgraph::GlobalConfig> gconfig_vector = std::make_shared<lgraph::GlobalConfig>();
-lgraph::StateMachine* ptr_state_machine_vector;
-extern void build_so();
-
-brpc::Server rpc_server_vector;
-struct Config : public lgraph::StateMachine::Config, public lgraph::RestServer::Config {
-    int verbose = 1;
-    std::string log_file;
-    bool enable_ha = false;
-    bool enable_rpc = true;
-    int thread_limit = 0;
-};
-class RPCService : public lgraph::LGraphRPCService {
- public:
-    explicit RPCService(lgraph::StateMachine* sm) : sm_(sm) {}
-
-    void HandleRequest(::google::protobuf::RpcController* controller,
-                       const ::lgraph::LGraphRequest* request, ::lgraph::LGraphResponse* response,
-                       ::google::protobuf::Closure* done) {
-        sm_->HandleRequest(controller, request, response, done);
-    }
-
- private:
-    lgraph::StateMachine* sm_;
-};
-RPCService* ptr_rpc_service_vector;
-
-void on_initialize_rpc_server_vector() {
-    using namespace fma_common;
-    using namespace lgraph;
-    bool enable_ssl = true;
-    std::string host = "127.0.0.1";
-    uint16_t port = 6464;
-    std::string db_name = "default";
-    {
-        Configuration config;
-        config.Add(enable_ssl, "ssl", true).Comment("Enable SSL");
-        config.Add(host, "host", true).Comment("Host address");
-        config.Add(port, "port", true).Comment("HTTP port");
-        config.Add(gconfig_vector->enable_ip_check,
-                    "enable_ip_check", true).Comment("Enable IP check.");
-    }
-
-    { GraphFactory::create_modern(); }
-    // Build listener's URI from the configured address and the hard-coded path "RestServer/Action"
-    sm_config_vector.db_dir = "./testdb_vector";
-    sm_config_vector.rpc_port = 19089;
-
-    gconfig_vector->ft_index_options.enable_fulltext_index = true;
-    lgraph::AccessControlledDB::SetEnablePlugin(true);
-    ptr_state_machine_vector = new lgraph::StateMachine(sm_config_vector, gconfig_vector);
-    ptr_rpc_service_vector = new RPCService(ptr_state_machine_vector);
-
-    rpc_server_vector.AddService(ptr_rpc_service_vector, brpc::SERVER_DOESNT_OWN_SERVICE);
-    rpc_server_vector.Start(sm_config_vector.rpc_port, nullptr);
-
-    ptr_state_machine_vector->Start();
-}
-
-void on_shutdown_rpc_server_vector() {
-    try {
-        ptr_state_machine_vector->Stop();
-    } catch (std::exception& e) {
-        LOG_ERROR() << "Rest server shutdown failed: " << e.what();
-    }
-    rpc_server_vector.Stop(0);
-}
-
-void* test_vector_rpc_server(void*) {
-    std::unique_lock<std::mutex> l(lock_rpc_vector);
-    if (stage_3_vector == 0) {
-        on_initialize_rpc_server_vector();
-        UT_LOG() << "rpc server is running";
-        stage_3_vector++;
-        cond_vector.notify_one();
-    }
-    if (stage_3_vector != 2) cond_vector.wait(l);
-    on_shutdown_rpc_server_vector();
-    UT_LOG() << __func__ << " thread exit";
-    delete ptr_state_machine_vector;
-    delete ptr_rpc_service_vector;
-    return nullptr;
-}
-
 int ElementCount_vector(const web::json::value& val,
                         const std::string& value, const std::string& field) {
     int count = 0;
@@ -213,20 +125,37 @@ int ElementCount_vector(const web::json::value& val,
     return count;
 }
 
-void test_vector_index(lgraph::RpcClient& client) {
+TEST_F(TestVsag, VectorProcedure) {
+    using namespace lgraph;
+    lgraph::GlobalConfig conf;
+    conf.db_dir = "./testdb";
+    conf.http_port = 7774;
+    conf.enable_rpc = true;
+    conf.rpc_port = 9394;
+    conf.bind_host = "127.0.0.1";
+#ifdef __SANITIZE_ADDRESS__
+    conf.use_pthread = true;
+#endif
+
+    AutoCleanDir cleaner(conf.db_dir);
+    auto server = StartLGraphServer(conf);
+    // create graphs
+    RpcClient client(UT_FMT("{}:{}", conf.bind_host, conf.rpc_port),
+                     _detail::DEFAULT_ADMIN_NAME, _detail::DEFAULT_ADMIN_PASS);
     UT_LOG() << "test AddVectorIndex , DeleteVectorIndex , ShowVectorIndex , VectorIndexQuery";
     std::string str;
     // vector.AddVectorIndex test
     bool ret = client.CallCypher(str,
-    "CALL db.createVertexLabel('person','id','id','int64',false,'vector','float_vector',true)");
+                                 "CALL db.createVertexLabel("
+                                 "'person','id','id','int64',false,'vector','float_vector',true)");
     UT_EXPECT_TRUE(ret);
     ret = client.CallCypher(str,
-    "CALL vector.AddVectorIndex('person','vector','HNSW',4,'L2',24,100)");
+                            "CALL vector.AddVectorIndex('person','vector','HNSW',4,'L2',24,100)");
     UT_EXPECT_TRUE(ret);
 
     // vector.ShowVectorIndex test
     ret = client.CallCypher(str,
-    "CALL vector.ShowVectorIndex()");
+                            "CALL vector.ShowVectorIndex()");
     UT_EXPECT_TRUE(ret);
     web::json::value json_val = web::json::value::parse(str);
     UT_EXPECT_TRUE(ret);
@@ -234,13 +163,13 @@ void test_vector_index(lgraph::RpcClient& client) {
 
     // vector.DeleteVectorIndex test
     ret = client.CallCypher(str,
-    "CALL vector.DeleteVectorIndex('person','vector','HNSW',4,'L2')");
+                            "CALL vector.DeleteVectorIndex('person','vector','HNSW',4,'L2')");
     UT_EXPECT_TRUE(ret);
     ret = client.CallCypher(str,
-    "CALL vector.AddVectorIndex('person','vector','HNSW',4,'L2',24,100)");
+                            "CALL vector.AddVectorIndex('person','vector','HNSW',4,'L2',24,100)");
     UT_EXPECT_TRUE(ret);
     ret = client.CallCypher(str,
-    "CALL vector.ShowVectorIndex()");
+                            "CALL vector.ShowVectorIndex()");
     UT_EXPECT_TRUE(ret);
     json_val = web::json::value::parse(str);
     UT_EXPECT_TRUE(ret);
@@ -248,58 +177,33 @@ void test_vector_index(lgraph::RpcClient& client) {
 
     // vector.VectorIndexQuery test
     ret = client.CallCypher(str,
-    "CREATE (n:person {id:1, vector: [1.0,1.0,1.0,1.0]})");
+                            "CREATE (n:person {id:1, vector: [1.0,1.0,1.0,1.0]})");
     UT_EXPECT_TRUE(ret);
     ret = client.CallCypher(str,
-    "CREATE (n:person {id:2, vector: [2.0,2.0,2.0,2.0]})");
+                            "CREATE (n:person {id:2, vector: [2.0,2.0,2.0,2.0]})");
     UT_EXPECT_TRUE(ret);
     ret = client.CallCypher(str,
-    R"(CALL db.upsertVertexByJson('person', '[{"id":3, "vector": [3,3,3,3]},
+                            R"(CALL db.upsertVertexByJson('person', '[{"id":3, "vector": [3,3,3,3]},
             {"id":4, "vector": [4,4,4,4]}]'))");
     UT_EXPECT_TRUE(ret);
     ret = client.CallCypher(str,
-    "CALL db.upsertVertex('person', [{id:5, vector: [5.0,5.0,5.0,5.0]},"
+                            "CALL db.upsertVertex('person', [{id:5, vector: [5.0,5.0,5.0,5.0]},"
                             "{id:6, vector: [6.0,6.0,6.0,6.0]}])");
     UT_EXPECT_TRUE(ret);
     ret = client.CallCypher(str,
-    "CALL vector.VectorIndexQuery('person','vector',[1,2,3,4], 2, 10)");
+                            "CALL vector.VectorIndexQuery('person','vector',[1,2,3,4], 2, 10)");
     UT_EXPECT_TRUE(ret);
     json_val = web::json::value::parse(str);
     UT_EXPECT_TRUE(ret);
-    UT_EXPECT_EQ(ElementCount_vector(json_val,
-    "3.000000,3.000000,3.000000,3.000000", "vec"), 1);
-    UT_EXPECT_EQ(ElementCount_vector(json_val,
-    "2.000000,2.000000,2.000000,2.000000", "vec"), 1);
+    UT_EXPECT_EQ(json_val[0]["vec"].as_string(), "2.000000,2.000000,2.000000,2.000000");
+
+    UT_EXPECT_EQ(json_val[1]["vec"].as_string(), "3.000000,3.000000,3.000000,3.000000");
     ret = client.CallCypher(str,
-         "CALL vector.VectorIndexQuery('person','vector',[1,2,3,4], 2, 10) YIELD vid");
+                            "CALL vector.VectorIndexQuery("
+                            "'person','vector',[1,2,3,4], 2, 10) YIELD vid");
     UT_EXPECT_TRUE(ret);
     UT_EXPECT_EQ(json_val[0]["vid"], 1);
     UT_EXPECT_EQ(json_val[1]["vid"], 2);
     ret = client.CallCypher(str, "CALL db.dropDB");
     UT_EXPECT_TRUE(ret);
-}
-
-void* test_vector_rpc_client(void*) {
-    using namespace lgraph;
-    std::unique_lock<std::mutex> l(lock_rpc_vector);
-    if (stage_3_vector == 0) cond_vector.wait(l);
-    // start test user login
-    UT_LOG() << "admin user login";
-    {
-        RpcClient client3("0.0.0.0:19089", "admin", "73@TuGraph");
-        test_vector_index(client3);
-    }
-
-    stage_3_vector++;
-    cond_vector.notify_one();
-    UT_LOG() << __func__ << " thread exit";
-    return nullptr;
-}
-
-TEST_F(TestVsag, VectorProcedure) {
-    // fma_common::Logger::Get().SetLevel(fma_common::LogLevel::LL_DEBUG);
-    std::thread tid_https[2] = {std::thread(test_vector_rpc_server, nullptr),
-                                std::thread(test_vector_rpc_client, nullptr)};
-    tid_https[0].join();
-    tid_https[1].join();
 }
