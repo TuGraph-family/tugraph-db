a659d66c92e56f468b6621194205c4b59522e1e3
lipanpan03 <656461146@qq.com>
2024-09-23 16:29:10 +0800

    fix procedure

--- procedures/make_custom_embed.sh
+++ procedures/make_custom_embed.sh
@@ -11,12 +11,13 @@
 # run or debug.
 
 APP=$1
+PROCEDURE_PATH=${2:-algo_cpp}
 APP_PATH=algo_cpp
 LG_INCLUDE=../include
 LG_LIB=../build/output/
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    g++ -fno-gnu-unique -fPIC -g --std=c++17 -I${LG_INCLUDE} -rdynamic -O3 -fopenmp -ldl -DNDEBUG -o $APP_PATH/${APP}_procedure $APP_PATH/${APP}_core.cpp $APP_PATH/${APP}_procedure.cpp embed_main.cpp "${LG_LIB}/liblgraph.so" -lrt
+    g++ -fno-gnu-unique -fPIC -g --std=c++17 -I${LG_INCLUDE} -rdynamic -O3 -fopenmp -ldl -DNDEBUG -o $PROCEDURE_PATH/${APP}_procedure $APP_PATH/${APP}_core.cpp $PROCEDURE_PATH/${APP}_procedure.cpp embed_main.cpp "${LG_LIB}/liblgraph.so" -lrt
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    clang++ -stdlib=libc++ -fPIC -g --std=c++17 -I${LG_INCLUDE} -rdynamic -O3 -Xpreprocessor -fopenmp -ldl -lomp -DNDEBUG -o $APP_PATH/${APP}_procedure $APP_PATH/${APP}_core.cpp $APP_PATH/${APP}_procedure.cpp embed_main.cxx "${LG_LIB}/liblgraph.so"
+    clang++ -stdlib=libc++ -fPIC -g --std=c++17 -I${LG_INCLUDE} -rdynamic -O3 -Xpreprocessor -fopenmp -ldl -lomp -DNDEBUG -o $PROCEDURE_PATH/${APP}_procedure $APP_PATH/${APP}_core.cpp $PROCEDURE_PATH/${APP}_procedure.cpp embed_main.cxx "${LG_LIB}/liblgraph.so"
 fi
