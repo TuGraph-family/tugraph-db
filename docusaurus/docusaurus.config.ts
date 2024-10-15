68159556b5cf252750b0be7223b125fe71194449
wanzhongyun <wanzhongyun.wzy@antgroup.com>
2024-10-15 17:30:28 +0800

    feat: modify github workflow config file1

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tugraph-db',
+  baseUrl: '/tugraph-db/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
