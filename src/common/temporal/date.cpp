c318e1b738e2cae77782565eb02c09270c42fc98
Junwang Zhao <zhjwpku@gmail.com>
2024-11-08 08:49:44 +0000

    construct Date from map
    Signed-off-by: Junwang Zhao <zhjwpku@gmail.com>

--- src/common/temporal/date.cpp
+++ src/common/temporal/date.cpp
@@ -12,7 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
+#include <set>
+
 #include <date/tz.h>
+#include <date/iso_week.h>
 
 #include "common/exceptions.h"
 #include "common/temporal/date.h"
@@ -45,6 +48,87 @@ constexpr int LONG_DOQ = 15;
 constexpr int LONG_DOY = 16;
 
 const std::regex DATE_REGEX(DATE_PATTERN);
+
+enum class DateType {
+    CALENDER_DATE = 0,
+    WEEK_DATE = 1,
+    QUATER_DATE = 2,
+    ORDINAL_DATE = 3
+};
+
+const std::string DATE_TIMEZONE = "timezone";
+const std::string DATE_DATE = "date";
+const std::string DATE_YEAR = "year";
+const std::string DATE_MONTH = "month";
+const std::string DATE_DAY = "day";
+const std::string DATE_WEEK = "week";
+const std::string DATE_DOW = "dayofweek";
+const std::string DATE_QUARTER = "quarter";
+const std::string DATE_DOQ = "dayofquarter";
+const std::string DATE_ORDINAL = "ordinalday";
+
+constexpr size_t DATE_FIELD_CNT = 10;
+
+const std::vector<std::string> validDateFields{
+    DATE_TIMEZONE, DATE_DATE, DATE_YEAR,    DATE_MONTH, DATE_DAY,
+    DATE_WEEK,     DATE_DOW,  DATE_QUARTER, DATE_DOQ,   DATE_ORDINAL};
+
+void validateDateParams(
+    const std::unordered_map<std::string, Value>& params_map) {
+    const static std::unordered_map<
+        std::string, std::pair<std::set<std::string>, std::string>>
+        fieldConflictMap{
+            {DATE_WEEK,
+             {{DATE_MONTH, DATE_ORDINAL, DATE_DAY, DATE_DOQ, DATE_QUARTER},
+              "Cannot assign {} to week date."}},
+            {DATE_MONTH,
+             {{DATE_QUARTER, DATE_ORDINAL},
+              "Cannot assign {} to calendar date."}},
+            {DATE_DAY, {{DATE_QUARTER}, "Cannot assign {} to calendar date."}},
+            {DATE_DOQ, {{DATE_MONTH}, "Cannot assign {} to quarter date."}},
+            {DATE_ORDINAL,
+             {{DATE_DAY, DATE_QUARTER}, "Cannot assign {} to ordinal date."}}};
+
+    const static std::unordered_map<std::string, std::string> fieldGroup{
+        {DATE_DAY, DATE_MONTH},
+        {DATE_DOQ, DATE_QUARTER},
+        {DATE_DOW, DATE_WEEK}};
+
+    if (params_map.count(DATE_TIMEZONE) && params_map.size() > 1) {
+        THROW_CODE(InputError,
+                   "Cannot assign time zone if also assigning other fields.");
+    }
+
+    // handle field conflict
+    for (const auto& [param, _] : params_map) {
+        if (fieldConflictMap.count(param)) {
+            const auto& fieldConflit = fieldConflictMap.at(param);
+            for (const auto& rhs : fieldConflit.first) {
+                if (params_map.count(rhs)) {
+                    THROW_CODE(InputError, fieldConflit.second.c_str(), rhs);
+                }
+            }
+        }
+    }
+
+    // `year` must be specified, except for ordinal day
+    if (params_map.count(DATE_YEAR) == 0 &&
+        (params_map.count(DATE_ORDINAL) == 0 &&
+         params_map.count(DATE_DATE) == 0)) {
+        THROW_CODE(InputError, "year must be specified");
+    }
+
+    // handle field group
+    for (const auto& [param, _] : params_map) {
+        if (fieldGroup.count(param)) {
+            if (params_map.count(fieldGroup.at(param)) == 0) {
+                THROW_CODE(InputError, "{} can not be specified without {}",
+                           param, fieldGroup.at(param));
+            }
+        }
+    }
+}
+
 }  // namespace
 
 namespace common {
@@ -62,21 +146,42 @@ void Date::fromYearMonthDay(int year, unsigned month, unsigned day) {
     }
 }
 
-void Date::fromYearWeekDow(int year, int week, int dow) {
-    auto ymd = date::year{year} / 1 / 1;
-    if (ymd.ok()) {
-        date::local_days ld = (date::local_days)ymd;
-        auto wkd = date::weekday(ld);
-        auto wkd_index = wkd.c_encoding();
-        int days = (week - 1) * 7 + dow - wkd_index;
-        if (days < 0) {
-            fromYearMonthDay(year - 1, 12, 31 + days + 1);
-        } else {
-            fromYearDoy(year, days + 1);
-        }
+void Date::fromYearWeekDow(int year, unsigned week, unsigned dow) {
+    iso_week::year_weeknum_weekday yww(iso_week::year{year},
+                                       iso_week::weeknum{week},
+                                       iso_week::weekday{date::weekday{dow}});
+
+    if (yww.ok()) {
+        date::local_days ld = date::local_days(yww);
+        days_since_epoch_ = ld.time_since_epoch().count();
     } else {
         std::ostringstream oss;
-        oss << ymd;
+        oss << yww;
+        THROW_CODE(InputError, "{}", oss.str());
+    }
+}
+
+void Date::fromYearWeekDow(const Date& base_date, std::optional<int> year,
+                           std::optional<unsigned> week,
+                           std::optional<unsigned> dow) {
+    iso_week::year_weeknum_weekday yww(
+        date::local_days{date::days{base_date.GetStorage()}});
+
+    int target_year = year.has_value() ? year.value() : (int)yww.year();
+    unsigned target_week =
+        week.has_value() ? week.value() : (unsigned)yww.weeknum();
+    unsigned target_dow =
+        dow.has_value() ? dow.value() : (unsigned)yww.weekday();
+
+    yww = iso_week::year_weeknum_weekday(
+        iso_week::year{target_year}, iso_week::weeknum{target_week},
+        iso_week::weekday{date::weekday{target_dow}});
+    if (yww.ok()) {
+        date::local_days ld = date::local_days(yww);
+        days_since_epoch_ = ld.time_since_epoch().count();
+    } else {
+        std::ostringstream oss;
+        oss << yww;
         THROW_CODE(InputError, "{}", oss.str());
     }
 }
@@ -148,7 +253,7 @@ Date::Date() {
 Date::Date(const std::string& str) {
     int year = 0, doy = 1;
     unsigned month = 1, day = 1;
-    int week = 1, dow = 1;
+    unsigned week = 1, dow = 1;
     int quarter = 1, doq = 1;
 
     std::smatch match;
@@ -216,7 +321,90 @@ Date::Date(const std::string& str) {
     }
 }
 
-Date::Date(const Value& params) { auto& params_map = params.AsMap(); }
+Date::Date(const Value& params) {
+    std::optional<int> year;
+    std::optional<unsigned> week;
+    std::optional<unsigned> dow;
+    int doy = 1;
+    unsigned month = 1, day = 1;
+    int quarter = 1, doq = 1;
+
+    std::optional<Date> base_date = std::nullopt;
+    std::unordered_map<std::string, Value> params_map;
+    for (const auto& kv : params.AsMap()) {
+        auto s = kv.first;
+        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+        if (std::find(validDateFields.begin(), validDateFields.end(), s) ==
+            validDateFields.end()) {
+            THROW_CODE(InputError, "No such field: {}", kv.first);
+        }
+        if (!params_map.emplace(s, kv.second).second) {
+            THROW_CODE(InputError, "Cannot re-assign {}", s);
+        }
+    }
+    validateDateParams(params_map);
+
+    DateType dt = DateType::CALENDER_DATE;
+    try {
+        if (params_map.count(std::string(DATE_YEAR))) {
+            year = (int)params_map.at(DATE_YEAR).AsInteger();
+        }
+        if (params_map.count(DATE_MONTH)) {
+            month = (unsigned)params_map.at(DATE_MONTH).AsInteger();
+        }
+        if (params_map.count(DATE_DAY)) {
+            day = (unsigned)params_map.at(DATE_DAY).AsInteger();
+        }
+        if (params_map.count(DATE_WEEK)) {
+            week = (int)params_map.at(DATE_WEEK).AsInteger();
+            dt = DateType::WEEK_DATE;
+        }
+        if (params_map.count(DATE_DOW)) {
+            dow = (int)params_map.at(DATE_DOW).AsInteger();
+        }
+        if (params_map.count(DATE_QUARTER)) {
+            quarter = (int)params_map.at(DATE_QUARTER).AsInteger();
+            dt = DateType::QUATER_DATE;
+        }
+        if (params_map.count(DATE_DOQ)) {
+            doq = (int)params_map.at(DATE_DOQ).AsInteger();
+        }
+        if (params_map.count(DATE_ORDINAL)) {
+            doy = (int)params_map.at(DATE_ORDINAL).AsInteger();
+            dt = DateType::ORDINAL_DATE;
+        }
+        if (params_map.count(DATE_DATE)) {
+            base_date = params_map.at(DATE_DATE).AsDate();
+        }
+    } catch (const std::exception& e) {
+        THROW_CODE(InputError, "Failed to parse {} into Date, exception: {}",
+                   params.ToString(), e.what());
+    }
+
+    switch (dt) {
+        case DateType::CALENDER_DATE:
+            fromYearMonthDay(year.value(), month, day);
+            break;
+        case DateType::WEEK_DATE: {
+            if (base_date.has_value()) {
+                fromYearWeekDow(base_date.value(), year, week, dow);
+            } else {
+                fromYearWeekDow(year.value(), week.value(), dow.value_or(1));
+            }
+            break;
+        }
+        case DateType::QUATER_DATE:
+            fromYearQuarterDoq(year.value(), quarter, doq);
+            break;
+        case DateType::ORDINAL_DATE:
+            fromYearDoy(year.value(), doy);
+            break;
+        default:
+            THROW_CODE(InputError, "Failed to parse {} into Date",
+                       params.ToString());
+            break;
+    }
+}
 
 std::string Date::ToString() const {
     date::local_days tp((date::days(days_since_epoch_)));
