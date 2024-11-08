5bf6568bf1fdec7a3f6698de9b331a41134748c8
liukaiming-alipay <lkm01250564@antgroup.com>
2024-11-08 10:02:26 +0800

    feat: TuGraph_Analytics url

--- docusaurus/src/theme/DocSidebar/index.tsx
+++ docusaurus/src/theme/DocSidebar/index.tsx
@@ -74,7 +74,7 @@ export default function DocSidebarWrapper(props: Props): JSX.Element {
     const [type, version] = values
     console.log('version', version, type)
     if (type === 'TuGraph_Analytics') {
-      window.location.href = 'https://liukaiming-alipay.github.io/tugraph-analytics/zh/introduction/';
+      window.location.href = `https://liukaiming-alipay.github.io/tugraph-analytics/${getCurrentLanguage()}/introduction/`;
       return
     }
 
