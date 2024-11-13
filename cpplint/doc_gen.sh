88f393ccf8f255a41ec85112afa830f3306d4d87
lipanpan03 <656461146@qq.com>
2024-11-13 11:13:49 +0800

    add_doc_in_centos7

--- cpplint/doc_gen.sh
+++ cpplint/doc_gen.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -e
 yum install doxygen -y
-python -m pip install --exists-action=w --no-cache-dir -r docs/requirements.txt
+pip install --exists-action=w --no-cache-dir -r docs/requirements.txt
 cd docs/zh-CN
 #make html SPHINXOPTS=-W
 make html
