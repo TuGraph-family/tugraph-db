051d63ef55d6c46f99fed0e5800537272c54c0b1
ZhaoYingqi <ferrarizyq@126.com>
2024-11-26 18:31:11 +0800

    Revised the open source license declaration.

--- procedures/community/khop_core.cpp
+++ procedures/community/khop_core.cpp
@@ -1,21 +1,19 @@
-/**
+/*
  * Copyright 2024 Yingqi Zhao
- * 
- * @Function: Compute the k-hop algorithm.
- * @param:
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *
- graph:
- The graph to compute on.
- root_vid: Identifier of root node.
- result:
- Set of reachable nodes in K-hop.
- value_k: Number of search layers.
- * @return: Number of nodes in K_hop.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-// #include <cstddef>
+
 #include "lgraph/olap_base.h"
 #include "./algo.h"
 
